062274557f3640733a8769d633f978efca6ad531
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2023-03-15 13:20:09 +0000

    Communication support added. Added duplicate check and corrected Communication examples.

--- src/main/kotlin/uk/nhs/england/tie/provider/QuestionnaireResponseProvider.kt
+++ src/main/kotlin/uk/nhs/england/tie/provider/QuestionnaireResponseProvider.kt
@@ -5,17 +5,23 @@ import ca.uhn.fhir.rest.api.MethodOutcome
 
 import ca.uhn.fhir.rest.param.ReferenceParam
 import ca.uhn.fhir.rest.param.TokenParam
+import ca.uhn.fhir.rest.param.UriParam
 import ca.uhn.fhir.rest.server.IResourceProvider
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException
 import org.hl7.fhir.r4.model.*
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent
 import org.springframework.stereotype.Component
+import uk.nhs.england.tie.awsProvider.AWSQuestionnaire
 import uk.nhs.england.tie.awsProvider.AWSQuestionnaireResponse
+import java.util.*
 
 
 import javax.servlet.http.HttpServletRequest
 
 @Component
 class QuestionnaireResponseProvider(
-                                   var awsQuestionnaireResponse: AWSQuestionnaireResponse
+                                   var awsQuestionnaireResponse: AWSQuestionnaireResponse,
+    var awsQuestionnaire: AWSQuestionnaire
 
 ) : IResourceProvider {
     override fun getResourceType(): Class<QuestionnaireResponse> {
@@ -28,6 +34,68 @@ class QuestionnaireResponseProvider(
         @ResourceParam questionnaireResponse: QuestionnaireResponse
     ): MethodOutcome? {
         return awsQuestionnaireResponse.createUpdate(questionnaireResponse)
+    }
+
+    @Operation(name = "\$extract", idempotent = true)
+    fun expand(@ResourceParam questionnaireResponse: QuestionnaireResponse
+    ): Bundle {
+        var bundle: Bundle = Bundle();
+        bundle.type = Bundle.BundleType.TRANSACTION;
+        if (!questionnaireResponse.hasQuestionnaire()) throw UnprocessableEntityException("Questionnaire must be supplied");
+        val questionnaire = awsQuestionnaire.search(UriParam().setValue(questionnaireResponse?.questionnaire))
+        if (questionnaire == null || questionnaire.size==0) throw UnprocessableEntityException("Questionnaire not found");
+        for(item in questionnaireResponse.item) {
+           var questionItem = getItem(questionnaire[0], item.linkId)
+
+            if (questionItem.hasCode() && item.answerFirstRep != null) {
+                var observation = Observation()
+                observation.status = Observation.ObservationStatus.FINAL;
+                if (questionnaireResponse.hasIdentifier()) {
+                    var identifier = Identifier()
+                    identifier.system = questionnaireResponse.identifier.system
+                    identifier.value = questionnaireResponse.identifier.value + item.linkId
+                    observation.addIdentifier(identifier)
+                }
+                if (questionnaireResponse.hasAuthor()) {
+                    observation.addPerformer(questionnaireResponse.author)
+                }
+                observation.code = CodeableConcept()
+                    observation.code.coding = questionItem.code
+                for (answer in item.answer) {
+                    if (answer.hasValueQuantity()) {
+                        observation.setValue(answer.valueQuantity)
+                    }
+                    if (answer.hasValueCoding()) {
+                        observation.setValue(answer.valueCoding)
+                    }
+                    if (answer.hasValueDecimalType()) {
+                        observation.setValue(Quantity().setValue(answer.valueDecimalType.value))
+                    }
+                    if (answer.hasValueIntegerType()) {
+                        observation.setValue(answer.valueIntegerType)
+                    }
+                }
+                observation.setSubject(questionnaireResponse.subject)
+                if (questionnaireResponse.hasAuthored()) {
+                    observation.setEffective(DateTimeType().setValue(questionnaireResponse.authored ))
+                }
+                var entry = BundleEntryComponent()
+                var uuid = UUID.randomUUID();
+                entry.fullUrl = "urn:uuid:" + uuid.toString()
+                entry.resource = observation
+                entry.request.url = "Observation"
+                entry.request.method = Bundle.HTTPVerb.POST
+                bundle.entry.add(entry)
+            }
+        }
+        return bundle;
+    }
+
+    private fun getItem(questionnaire: Questionnaire, linkId: String): Questionnaire.QuestionnaireItemComponent {
+        for (item in questionnaire.item) {
+            if (linkId.equals(item.linkId)) return item;
+        }
+        throw UnprocessableEntityException("linkId not found " + linkId)
     }
 
 
