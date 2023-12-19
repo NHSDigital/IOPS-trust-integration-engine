79c001947a122666f45e9d1a4e7c0b5da0119f45
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2023-12-19 11:53:47 +0000

    FHIR Document code copied from https://github.com/nhsconnect/careconnect-examples/tree/master/hapi-fhirDocument-create

---
+++ src/main/kotlin/uk/nhs/england/tie/component/FhirBundleUtil.kt
@@ -0,0 +1,232 @@
+package uk.nhs.england.tie.component
+
+import org.hl7.fhir.r4.model.*
+import org.slf4j.LoggerFactory
+import java.util.*
+
+class FhirBundleUtil internal constructor(value: Bundle.BundleType?) {
+    // THIS IS A LIBRARY CLASS !!!BUT!!! IS CURRENTLY IN SEVERAL MODULES
+    // BE CAREFUL AND UPDATE ALL VERSIONS.
+    //
+    // This will be moved to a central library when development is at that stage
+    var fhirDocument: Bundle = Bundle()
+        .setType(value)
+    var patient: Patient? = null
+    var referenceMap: MutableMap<String, String?> = HashMap()
+    val uuidtag = "urn:uuid:"
+
+    init {
+        fhirDocument.getIdentifier().setValue(UUID.randomUUID().toString())
+            .setSystem("https://tools.ietf.org/html/rfc4122")
+    }
+
+
+
+    fun processReferences() {
+        for (entry in fhirDocument!!.entry) {
+            if (entry.resource is AllergyIntolerance) {
+                val allergyIntolerance = entry.resource as AllergyIntolerance
+                allergyIntolerance.setPatient(getUUIDReference(allergyIntolerance.patient))
+            }
+            if (entry.resource is Condition) {
+                val condition = entry.resource as Condition
+                condition.setSubject(getUUIDReference(condition.subject))
+                if (condition.hasEncounter()) {
+                    condition.setEncounter(getUUIDReference(condition.encounter))
+                }
+            }
+            if (entry.resource is Composition) {
+                val composition = entry.resource as Composition
+                if (composition.hasSubject()) composition.setSubject(getUUIDReference(composition.subject))
+                for (attester in composition.attester) {
+                    if (attester.hasParty()) {
+                        attester.setParty(getUUIDReference(attester.party))
+                    }
+                }
+                for (reference in composition.author) {
+                    reference.setReference(getUUIDReference(reference)!!.reference)
+                }
+            }
+            if (entry.resource is DocumentReference) {
+                val documentReference = entry.resource as DocumentReference
+                if (documentReference.hasSubject()) {
+                    documentReference.setSubject(getUUIDReference(documentReference.subject))
+                }
+                if (documentReference.hasContent()) {
+                    for (content in documentReference.content) if (content.hasAttachment()) {
+                        log.debug("Attachment url = " + content.attachment.url)
+                        content.attachment.setUrl(uuidtag + getNewReferenceUri(content.attachment.url))
+                    }
+                }
+                if (documentReference.hasCustodian()) {
+                    // log.info("Bundle Custodian Ref = "+documentReference.getCustodian().getReference());
+                    documentReference.setCustodian(getUUIDReference(documentReference.custodian))
+                }
+                for (reference in documentReference.author) {
+                    reference.setReference(getUUIDReference(reference)!!.reference)
+                }
+            }
+            if (entry.resource is Device) {
+                val device = entry.resource as Device
+                if (device.hasOwner()) {
+                    device.setOwner(getUUIDReference(device.owner))
+                }
+            }
+            if (entry.resource is Encounter) {
+                val encounter = entry.resource as Encounter
+                encounter.setSubject(Reference(uuidtag + patient!!.id))
+                if (encounter.hasServiceProvider() && encounter.serviceProvider.reference != null) {
+                    encounter.setServiceProvider(getUUIDReference(encounter.serviceProvider))
+                } else {
+                    encounter.setServiceProvider(null)
+                }
+                val newReferences: MutableList<Reference> = ArrayList()
+                for (reference in encounter.episodeOfCare) {
+                    val newReference = getUUIDReference(reference)
+                    if (newReference != null) {
+                        newReferences.add(newReference)
+                    }
+                }
+                encounter.setEpisodeOfCare(newReferences)
+                for (locationComponent in encounter.location) {
+                    locationComponent.setLocation(getUUIDReference(locationComponent.location))
+                }
+                for (participantComponent in encounter.participant) {
+                    if (participantComponent.hasIndividual()) participantComponent.setIndividual(
+                        getUUIDReference(
+                            participantComponent.individual
+                        )
+                    )
+                }
+            }
+            if (entry.resource is Observation) {
+                val observation = entry.resource as Observation
+                observation.setSubject(Reference(uuidtag + patient!!.id))
+                if (observation.hasEncounter()) {
+                    observation.setEncounter(getUUIDReference(observation.encounter))
+                }
+            }
+            if (entry.resource is MedicationRequest) {
+                val medicationRequest = entry.resource as MedicationRequest
+                if (medicationRequest.hasEncounter()) {
+                    medicationRequest.setEncounter(getUUIDReference(medicationRequest.encounter))
+                }
+                medicationRequest.setSubject(Reference(uuidtag + patient!!.id))
+                if (medicationRequest.hasRecorder()) medicationRequest.setRecorder(getUUIDReference(medicationRequest.recorder))
+                try {
+                    if (medicationRequest.hasMedicationReference()) medicationRequest.setMedication(
+                        getUUIDReference(
+                            medicationRequest.medicationReference
+                        )
+                    )
+                } catch (ex: Exception) {
+                }
+            }
+            if (entry.resource is MedicationStatement) {
+                val medicationStatement = entry.resource as MedicationStatement
+                medicationStatement.setSubject(Reference(uuidtag + patient!!.id))
+                if (medicationStatement.hasContext()) {
+                    medicationStatement.setContext(getUUIDReference(medicationStatement.context))
+                }
+            }
+            if (entry.resource is Organization) {
+                val organization = entry.resource as Organization
+                if (organization.hasPartOf()) {
+                    if (getNewReferenceUri(organization.partOf.reference) != null) {
+                        organization.setPartOf(getUUIDReference(organization.partOf))
+                    } else {
+                        organization.setPartOf(null)
+                    }
+                }
+            }
+            if (entry.resource is Patient) {
+                val patient = entry.resource as Patient
+                if (patient.hasManagingOrganization()) {
+                    patient.setManagingOrganization(Reference(uuidtag + getNewReferenceUri(patient.managingOrganization.reference)))
+                }
+                for (reference in patient.generalPractitioner) {
+                    patient.generalPractitioner[0].setReference(uuidtag + getNewReferenceUri(reference.reference))
+                }
+            }
+            if (entry.resource is Practitioner) {
+                val practitioner = entry.resource as Practitioner
+            }
+            if (entry.resource is Procedure) {
+                val procedure = entry.resource as Procedure
+                procedure.setSubject(Reference(uuidtag + patient!!.id))
+                if (procedure.hasEncounter()) {
+                    procedure.setEncounter(getUUIDReference(procedure.encounter))
+                }
+            }
+        }
+    }
+
+    fun processBundleResources(bundle: Bundle) {
+        val gp: Practitioner? = null
+        val practice: Organization? = null
+        for (entry in bundle.entry) {
+            val resource = entry.resource
+            resource.setId(getNewId(resource))
+            fhirDocument!!.addEntry().setResource(entry.resource).setFullUrl(uuidtag + resource.id)
+            if (entry.resource is Patient) {
+                patient = entry.resource as Patient
+            }
+        }
+    }
+
+    private fun getUUIDReference(reference: Reference): Reference? {
+        if (referenceMap[reference.reference] == null) {
+            log.error("Missing reference " + reference.reference)
+        }
+        return if (reference.reference == getNewReferenceUri(reference.reference)) {
+            reference
+        } else {
+            val UUIDReference = getNewReferenceUri(reference.reference)
+            if (UUIDReference != null) {
+                Reference(uuidtag + UUIDReference)
+            } else {
+                null
+            }
+        }
+    }
+
+    private fun getNewReferenceUri(resource: Resource): String? {
+        return getNewReferenceUri(resource.resourceType.toString() + "/" + resource.id)
+    }
+
+    fun getNewId(resource: Resource): String {
+        var reference = resource.id
+        var newReference: String? = null
+        if (reference != null) {
+            newReference = referenceMap[reference]
+            if (newReference != null) return newReference
+        }
+        newReference = UUID.randomUUID().toString()
+        if (reference == null) {
+            reference = newReference
+            referenceMap[resource.javaClass.getSimpleName() + "/" + reference] = newReference
+        } else {
+            log.info(resource.javaClass.getSimpleName() + "/" + resource.idElement.idPart + " [-] " + newReference)
+            referenceMap[resource.javaClass.getSimpleName() + "/" + resource.idElement.idPart] = newReference
+        }
+        log.debug("$reference [-] $newReference")
+        referenceMap[reference] = newReference
+        referenceMap[newReference] = newReference // Add in self
+        referenceMap[uuidtag + newReference] = newReference // Add in self
+        return newReference
+    }
+
+    private fun getNewReferenceUri(reference: String): String? {
+        if (reference.contains(uuidtag)) {
+            return reference.replace(uuidtag, "")
+        }
+        val newReference = referenceMap[reference]
+        if (newReference != null) return newReference
+        log.info("Missing newReference for $reference")
+        return newReference
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(FhirBundleUtil::class.java)
+    }
+}
---
+++ src/main/kotlin/uk/nhs/england/tie/component/FhirDocumentApp.kt
@@ -0,0 +1,377 @@
+package uk.nhs.england.tie.component
+
+import ca.uhn.fhir.context.FhirContext
+import ca.uhn.fhir.model.api.Include
+import ca.uhn.fhir.rest.client.api.IGenericClient
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException
+import org.hl7.fhir.instance.model.api.IBaseBundle
+import org.hl7.fhir.r4.model.*
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
+import org.thymeleaf.TemplateEngine
+import java.io.*
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.stream.StreamSource
+import org.xhtmlrenderer.pdf.ITextRenderer
+
+
+@Component
+class FhirDocumentApp(val client: IGenericClient, @Qualifier("R4") val ctxFHIR : FhirContext, val templateEngine: TemplateEngine) {
+
+    private val contextClassLoader: ClassLoader
+        private get() = Thread.currentThread().getContextClassLoader()
+
+
+    var df: DateFormat = SimpleDateFormat("HHmm_dd_MM_yyyy")
+    var composition: Composition? = null
+    private var fhirBundleUtil: FhirBundleUtil = FhirBundleUtil(Bundle.BundleType.DOCUMENT)
+
+    /*
+    @Throws(Exception::class)
+    override fun run(vararg args: String) {
+        if (args.size > 0 && args[0] == "exitcode") {
+            throw Exception()
+        }
+        client = ctxFHIR.newRestfulGenericClient("https://data.developer.nhs.uk/ccri-fhir/STU3/")
+        client.setEncoding(EncodingEnum.XML)
+        outputCareRecord("1098")
+        //  outputCareRecord("1177");
+        val encounterBundle = buildEncounterDocument(client, IdType().setValue("1700"))
+        val date = Date()
+        val xmlResult = ctxFHIR.newXmlParser().setPrettyPrint(true).encodeResourceToString(encounterBundle)
+        Files.write(
+            Paths.get("/Temp/" + df.format(date) + "+encounter-" + "1700" + "-document.xml"),
+            xmlResult.toByteArray()
+        )
+    }*/
+
+    @Throws(Exception::class)
+    private fun outputCareRecord(patientId: String) {
+        val date = Date()
+        val careRecord = getCareRecord(patientId)
+        val xmlResult = this.ctxFHIR.newXmlParser().setPrettyPrint(true).encodeResourceToString(careRecord)
+        Files.write(
+            Paths.get("/Temp/" + df.format(date) + "+patientCareRecord-" + patientId + ".xml"),
+            xmlResult.toByteArray()
+        )
+        Files.write(
+            Paths.get("/Temp/" + df.format(date) + "+patientCareRecord-" + patientId + ".json"),
+            ctxFHIR.newJsonParser().setPrettyPrint(true).encodeResourceToString(careRecord).toByteArray()
+        )
+        /*
+        String htmlFilename = "/Temp/"+df.format(date)+"+patient-"+patientId+".html";
+        performTransform(xmlResult,htmlFilename,"XML/DocumentToHTML.xslt");
+        outputPDF(htmlFilename, "/Temp/"+df.format(date)+"+patient-"+patientId+".pdf");
+
+        IGenericClient clientTest = ctxFHIR.newRestfulGenericClient("http://127.0.0.1:8080/careconnect-gateway/STU3/");
+        clientTest.create().resource(careRecord).execute();
+        */
+    }
+
+    @Throws(Exception::class)
+    private fun outputPDF(processedHtml: String, outputFileName: String) {
+        var os: FileOutputStream? = null
+        val fileName = UUID.randomUUID().toString()
+        try {
+            val outputFile = File(outputFileName)
+            os = FileOutputStream(outputFile)
+            val renderer = ITextRenderer()
+            renderer.setDocument(File(processedHtml))
+            renderer.layout()
+            renderer.createPDF(os, false)
+            renderer.finishPDF()
+            //return outputFile.getAbsolutePath();
+        } finally {
+            if (os != null) {
+                try {
+                    os.close()
+                } catch (e: IOException) { /*ignore*/
+                }
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun buildEncounterDocument(client: IGenericClient, encounterId: IdType): Bundle {
+        fhirBundleUtil = FhirBundleUtil(Bundle.BundleType.DOCUMENT)
+        val compositionBundle = Bundle()
+
+        // Main resource of a FHIR Bundle is a Composition
+        composition = Composition()
+        composition!!.setId(UUID.randomUUID().toString())
+        compositionBundle.addEntry().setResource(composition)
+
+        // composition.getMeta().addProfile(CareConnectProfile.Composition_1);
+        composition!!.setTitle("Encounter Document")
+        composition!!.setDate(Date())
+        composition!!.setStatus(Composition.CompositionStatus.FINAL)
+        val leedsTH = getOrganization(client, "RR8")
+        compositionBundle.addEntry().setResource(leedsTH)
+        composition!!.addAttester()
+            .setParty(Reference("Organization/" + leedsTH!!.idElement.idPart))
+            .setMode(Composition.CompositionAttestationMode.OFFICIAL)
+        val device = Device()
+        device.setId(UUID.randomUUID().toString())
+        device.type.addCoding()
+            .setSystem("http://snomed.info/sct")
+            .setCode("58153004")
+            .setDisplay("Android")
+        device.setOwner(Reference("Organization/" + leedsTH.idElement.idPart))
+        compositionBundle.addEntry().setResource(device)
+        composition!!.addAuthor(Reference("Device/" + device.idElement.idPart))
+        composition!!.type.addCoding()
+            .setCode("371531000")
+            .setDisplay("Report of clinical encounter")
+            .setSystem(SNOMEDCT)
+        fhirBundleUtil.processBundleResources(compositionBundle)
+        fhirBundleUtil.processReferences()
+        val encounterBundle = getEncounterBundleRev(client, encounterId.idPart)
+        var encounter: Encounter? = null
+        for (entry in encounterBundle.entry) {
+            val resource = entry.resource
+            if (encounter == null && entry.resource is Encounter) {
+                encounter = entry.resource as Encounter
+            }
+        }
+        var patientId: String? = null
+        if (encounter != null) {
+            patientId = encounter.subject.referenceElement.idPart
+            log.debug(encounter.subject.referenceElement.idPart)
+
+
+            // This is a synthea patient
+            val patientBundle = getPatientBundle(client, patientId)
+            fhirBundleUtil.processBundleResources(patientBundle)
+            if (fhirBundleUtil.patient == null) throw Exception("404 Patient not found")
+            composition!!.setSubject(Reference("Patient/$patientId"))
+            patientId = fhirBundleUtil.patient!!.getId()
+        }
+        if (fhirBundleUtil.patient == null) throw UnprocessableEntityException()
+        fhirBundleUtil.processBundleResources(encounterBundle)
+        fhirBundleUtil.processReferences()
+        val fhirDoc = FhirDocUtil(templateEngine)
+        composition!!.addSection(fhirDoc.getEncounterSection(fhirBundleUtil.fhirDocument))
+        var section: Composition.SectionComponent = fhirDoc.getConditionSection(fhirBundleUtil.fhirDocument)
+        if (section.entry.size > 0) composition!!.addSection(section)
+        section = fhirDoc.getMedicationStatementSection(fhirBundleUtil.fhirDocument)
+        if (section.entry.size > 0) composition!!.addSection(section)
+        section = fhirDoc.getMedicationRequestSection(fhirBundleUtil.fhirDocument)
+        if (section.entry.size > 0) composition!!.addSection(section)
+        section = fhirDoc.getAllergySection(fhirBundleUtil.fhirDocument)
+        if (section.entry.size > 0) composition!!.addSection(section)
+        section = fhirDoc.getObservationSection(fhirBundleUtil.fhirDocument)
+        if (section.entry.size > 0) composition!!.addSection(section)
+        section = fhirDoc.getProcedureSection(fhirBundleUtil.fhirDocument)
+        if (section.entry.size > 0) composition!!.addSection(section)
+        return fhirBundleUtil.fhirDocument
+    }
+
+    @Throws(Exception::class)
+    private fun getCareRecord(patientId: String): Bundle {
+        // Create Bundle of type Document
+        var patientId = patientId
+        fhirBundleUtil = FhirBundleUtil(Bundle.BundleType.DOCUMENT)
+        // Main resource of a FHIR Bundle is a Composition
+        val compositionBundle = Bundle()
+        composition = Composition()
+        composition!!.setId(UUID.randomUUID().toString())
+        compositionBundle.addEntry().setResource(composition)
+        composition!!.setTitle("Patient Summary Care Record")
+        composition!!.setDate(Date())
+        composition!!.setStatus(Composition.CompositionStatus.FINAL)
+        val leedsTH = getOrganization(client, "RR8")
+        compositionBundle.addEntry().setResource(leedsTH)
+        composition!!.addAttester()
+            .setParty(Reference(leedsTH!!.id))
+            .setMode(Composition.CompositionAttestationMode.OFFICIAL)
+        val device = Device()
+        device.setId(UUID.randomUUID().toString())
+        device.type.addCoding()
+            .setSystem("http://snomed.info/sct")
+            .setCode("58153004")
+            .setDisplay("Android")
+        device.setOwner(Reference("Organization/" + leedsTH.idElement.idPart))
+        compositionBundle.addEntry().setResource(device)
+        composition!!.addAuthor(Reference("Device/" + device.idElement.idPart))
+        fhirBundleUtil.processBundleResources(compositionBundle)
+        fhirBundleUtil.processReferences()
+
+        // This is a synthea patient
+        val patientBundle = getPatientBundle(client, patientId)
+        fhirBundleUtil.processBundleResources(patientBundle)
+        if (fhirBundleUtil.patient == null) throw Exception("404 Patient not found")
+        composition!!.setSubject(Reference("Patient/$patientId"))
+        val fhirDoc = FhirDocUtil(templateEngine)
+        patientId = fhirBundleUtil.patient!!.getId()
+        fhirDoc.generatePatientHtml(fhirBundleUtil.patient, patientBundle)
+
+        /* CONDITION */
+        val conditionBundle = getConditionBundle(patientId)
+        fhirBundleUtil.processBundleResources(conditionBundle)
+        composition!!.addSection(fhirDoc.getConditionSection(conditionBundle))
+
+        /* MEDICATION STATEMENT */
+        val medicationStatementBundle = getMedicationStatementBundle(patientId)
+        fhirBundleUtil.processBundleResources(medicationStatementBundle)
+        composition!!.addSection(fhirDoc.getMedicationStatementSection(medicationStatementBundle))
+
+
+        /* ALLERGY INTOLERANCE */
+        val allergyBundle = getAllergyBundle(patientId)
+        fhirBundleUtil.processBundleResources(allergyBundle)
+        composition!!.addSection(fhirDoc.getAllergySection(allergyBundle))
+
+        /* ENCOUNTER */
+        val encounterBundle = getEncounterBundle(patientId)
+        fhirBundleUtil.processBundleResources(encounterBundle)
+        composition!!.addSection(fhirDoc.getEncounterSection(encounterBundle))
+        fhirBundleUtil.processReferences()
+        log.debug(ctxFHIR.newJsonParser().setPrettyPrint(true).encodeResourceToString(fhirBundleUtil.fhirDocument))
+        return fhirBundleUtil.fhirDocument
+    }
+
+    private fun getPatientBundle(client: IGenericClient?, patientId: String?): Bundle {
+        if (client != null) {
+            return client
+                .search<IBaseBundle>()
+                .forResource(Patient::class.java)
+                .where(Patient.RES_ID.exactly().code(patientId))
+                .include(Patient.INCLUDE_GENERAL_PRACTITIONER)
+                .include(Patient.INCLUDE_ORGANIZATION)
+                .returnBundle(Bundle::class.java)
+                .execute()
+        } else
+        return Bundle()
+    }
+
+    private fun getEncounterBundleRev(
+        client: IGenericClient?,
+        encounterId: String
+    ): Bundle {
+        if (client != null) {
+            return client
+                .search<IBaseBundle>()
+                .forResource(Encounter::class.java)
+                .where(Patient.RES_ID.exactly().code(encounterId))
+                .revInclude(Include("*"))
+                .include(Include("*"))
+                .count(100) // be careful of this TODO
+                .returnBundle(Bundle::class.java)
+                .execute()
+        } else {
+            return Bundle()
+        }
+    }
+
+    private fun getConditionBundle(patientId: String): Bundle {
+        return client
+            .search<IBaseBundle>()
+            .forResource(Condition::class.java)
+            .where(Condition.PATIENT.hasId(patientId))
+            .and(Condition.CLINICAL_STATUS.exactly().code("active"))
+            .returnBundle(Bundle::class.java)
+            .execute()
+    }
+
+    private fun getEncounterBundle(patientId: String): Bundle {
+        return client
+            .search<IBaseBundle>()
+            .forResource(Encounter::class.java)
+            .where(Encounter.PATIENT.hasId(patientId))
+            .count(3) // Last 3 entries same as GP Connect
+            .returnBundle(Bundle::class.java)
+            .execute()
+    }
+
+    private fun getOrganization(client: IGenericClient, sdsCode: String): Organization? {
+        var organization: Organization? = null
+        val bundle = client
+            .search<IBaseBundle>()
+            .forResource(Organization::class.java)
+            .where(Organization.IDENTIFIER.exactly().code(sdsCode))
+            .returnBundle(Bundle::class.java)
+            .execute()
+        if (bundle.entry.size > 0) {
+            if (bundle.entry[0].resource is Organization) organization = bundle.entry[0].resource as Organization
+        }
+        return organization
+    }
+
+    private fun getMedicationStatementBundle(patientId: String): Bundle {
+        return client
+            .search<IBaseBundle>()
+            .forResource(MedicationStatement::class.java)
+            .where(MedicationStatement.PATIENT.hasId(patientId))
+            .and(MedicationStatement.STATUS.exactly().code("active"))
+            .returnBundle(Bundle::class.java)
+            .execute()
+    }
+
+    private fun getMedicationRequestBundle(client: IGenericClient, patientId: String): Bundle {
+        return client
+            .search<IBaseBundle>()
+            .forResource(MedicationStatement::class.java)
+            .where(MedicationRequest.PATIENT.hasId(patientId))
+            .and(MedicationRequest.STATUS.exactly().code("active"))
+            .returnBundle(Bundle::class.java)
+            .execute()
+    }
+
+    private fun getAllergyBundle(patientId: String): Bundle {
+        return client
+            .search<IBaseBundle>()
+            .forResource(AllergyIntolerance::class.java)
+            .where(AllergyIntolerance.PATIENT.hasId(patientId))
+            .returnBundle(Bundle::class.java)
+            .execute()
+    }
+
+    private fun performTransform(xmlInput: String, htmlOutput: String, styleSheet: String) {
+
+        // Input xml data file
+        val classLoader = contextClassLoader
+
+        // Input xsl (stylesheet) file
+        val xslInput = classLoader.getResource(styleSheet).file
+
+        // Set the property to use xalan processor
+        System.setProperty(
+            "javax.xml.transform.TransformerFactory",
+            "org.apache.xalan.processor.TransformerFactoryImpl"
+        )
+
+        // try with resources
+        try {
+            val xml: InputStream = ByteArrayInputStream(xmlInput.toByteArray(StandardCharsets.UTF_8))
+            val os = FileOutputStream(htmlOutput)
+            val xsl = FileInputStream(xslInput)
+
+            // Instantiate a transformer factory
+            val tFactory = TransformerFactory.newInstance()
+
+            // Use the TransformerFactory to process the stylesheet source and produce a Transformer
+            val styleSource = StreamSource(xsl)
+            val transformer = tFactory.newTransformer(styleSource)
+
+            // Use the transformer and perform the transformation
+            val xmlSource = StreamSource(xml)
+            val result = StreamResult(os)
+            transformer.transform(xmlSource, result)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(FhirDocumentApp::class.java)
+        const val SNOMEDCT = "http://snomed.info/sct"
+    }
+}
